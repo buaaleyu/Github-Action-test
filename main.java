2978c023df0368131a3343b12bfa7446e8285cd6
buaaleyu <101405804+buaaleyu@users.noreply.github.com>
2022-03-11 22:38:32 +0800

    Update main.java

--- main.java
+++ main.java
@@ -2,20 +2,25 @@
 import java.util.Scanner;
 
 public class Main {
-    public boolean isPalindrome(int x) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
         if(x < 0 || (x % 10 == 0 && x != 0)){
-            return false;
+            System.out.println(false);
         }
         String str = String.valueOf(x);
         int L = str.length() - 1;
         int R = 0;
         while (L > R) {
-            if (str.charAt(L) != str.charAt(R))
-                return false;
+            if (str.charAt(L) != str.charAt(R)) {
+                System.out.println(false);
+            }
+            else {
+                System.out.println(true);
+            }
             L--;
             R++;
         }
-        return true;
+
     }
 }
